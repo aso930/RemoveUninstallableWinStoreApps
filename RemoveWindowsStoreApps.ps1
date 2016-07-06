6d84d817a07f8ce926638d35aac40bea4d0cfaa2
aso930 <alexandru.asofroniei@outlook.com>
2016-07-06 10:40:28 +0300

    Initial commit

---
+++ RemoveWindowsStoreApps.ps1
@@ -0,0 +1,38 @@
+﻿$appsarray = "WindowsFeedback", "ContactSupport", "windowscommunicationsapps", "WindowsCamera", "XboxApp", "ZuneMusic", "ZuneMusic", "ZuneVideo", "WindowsMaps", "WindowsSoundRecorder", "People", "CommsPhone", "WindowsAlarms"
+
+
+$title = "Remove Windows Store Apps"
+$message = "This will uninstall all the Windows Store apps in your PC."
+
+$yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", `
+    "Removes all the uninstallable apps"
+
+$no = New-Object System.Management.Automation.Host.ChoiceDescription "&No", `
+    "No changes are made."
+
+$options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
+
+$result = $host.ui.PromptForChoice($title, $message, $options, 0) 
+
+
+
+
+switch ($result)
+{
+    0{
+        foreach($app in $appsarray) 
+        {
+            echo $app
+            Get-AppxPackage -allusers *$app* | Remove-AppxPackage
+        }
+        }
+    1 {
+        echo "You selected No."
+       }
+    default {
+        exit
+        }
+}
+
+#Get-AppxPackage -allusers *WindowsFeedback* | Remove-AppxPackage
+#Get-AppxPackage -allusers *WindowsFeedback* | Remove-AppxPackage
